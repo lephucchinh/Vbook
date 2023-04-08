bece517c29f826cc2da9c6caaf03fb214b7d7940
lephucanh <phucanhdn01@gmail.com>
2023-04-08 17:47:46 +0700

    add new

--- lib/ui/tessstt.dart
+++ lib/ui/tessstt.dart
@@ -1,1 +1,10 @@
-// hello
+import 'package:flutter/material.dart';
+
+class tesst extends StatelessWidget {
+  const tesst({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
