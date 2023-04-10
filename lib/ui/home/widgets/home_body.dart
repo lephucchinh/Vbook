85e2725fad531e0c4f02df755dd49b2c0c87c7fe
Lê Phúc Chinh <lpchinh25@gmail.com>
2023-04-10 20:42:29 +0700

    vbook

--- lib/ui/home/widgets/home_body.dart
+++ lib/ui/home/widgets/home_body.dart
@@ -1,87 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:vbook/utils/data_noval.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
+    return GridView.builder(
       primary: true,
       padding: const EdgeInsets.all(10),
-      mainAxisSpacing: 30,
-      crossAxisSpacing: 10,
-      crossAxisCount: 3,
-      childAspectRatio: 1 / 2,
-      children: [
-        GestureDetector(
-          onTap: () {},
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/Cái này thích khách có bệnh.jpg',
-                fit: BoxFit.fill,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 1/2,
+        mainAxisSpacing: 3,
+        crossAxisSpacing: 10,
+      ),
+      itemCount: imagesUsing.novelPhoto.length,
+      itemBuilder: (context, int index) =>
+          GestureDetector(
+              child: Column(
+                  children: [
+                  imagesUsing.novelPhoto[index],
+                  Text(
+                    'Cái này thích khách có bệnh',
+                    textAlign: TextAlign.center,
+                  )
+                ],
               ),
-              Text(
-                'Cái này thích khách có bệnh',
-                textAlign: TextAlign.center,
-              )
-            ],
+
+            onTap: () {},
           ),
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Column(
-            children: const [
-              Image(
-                  image: AssetImage('assets/images/Hiệp đồ huyễn thế lục.jpg')),
-              Text(
-                "Hiệp đồ huyễn thế lục",
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Column(
-            children: const [
-              Image(
-                  image:
-                      AssetImage('assets/images/Kiếm hiệp phong vân trí.webp')),
-              Text(
-                "Kiếm hiệp phong vân trí",
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Column(
-            children: const [
-              Image(
-                  image: AssetImage(
-                      'assets/images/Thập phương loạn thế , nhân gian võ tánh.jpg')),
-              Text(
-                "Thập phương loạn thế , nhân gian võ tánh",
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Column(
-            children: const [
-              Image(image: AssetImage('assets/images/Tặc nhân hưu cẩu.webp')),
-              Text(
-                "Tặc nhân hưu cẩu",
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/utils/data_noval.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class imagesUsing {
+  static const List<Image> novelPhoto = [
+    Image(
+      image: AssetImage('assets/images/Cái này thích khách có bệnh.jpg'),
+      fit: BoxFit.fill,
+    ),
+    Image(
+      image: AssetImage('assets/images/Hiệp đồ huyễn thế lục.jpg'),
+      fit: BoxFit.fill,
+    ),
+    Image(
+      image: AssetImage("assets/images/Kiếm hiệp phong vân trí.webp"),
+      fit: BoxFit.fill,
+    ),
+    Image(
+      image: AssetImage(
+          "assets/images/Thập phương loạn thế , nhân gian võ tánh.jpg"),
+      fit: BoxFit.fill,
+    ),
+    Image(
+      image: AssetImage("assets/images/Tặc nhân hưu cẩu.webp"),
+      fit: BoxFit.fill,
+    ),
+  ];
+}
+
